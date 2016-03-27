cc907ebebc7a8172835c8add1e8101687efdfda1
Gerard Vico <gerard.vico@zyncro.com>
2016-03-27 20:07:32 +0200

    first commit

---
+++ src/main/java/com/hdbandit/starter_traceable/TraceableAutoConfiguration.java
@@ -0,0 +1,25 @@
+package com.hdbandit.starter_traceable;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import com.hdbandit.traceable.Tracer;
+import com.hdbandit.traceable.config.TraceableConfig;
+
+@Configuration
+@Import(TraceableConfig.class)
+public class TraceableAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Tracer defaultTracer() {
+        return new Tracer() {         
+            public void trace(String message) {
+                System.out.println(message);
+            }
+        };
+    }
+    
+}
